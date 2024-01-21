6b0d5e92d1e80b2d980d835309b9a2472f03d314
KIRUTHIYAASHREE S P <98012434+kiruthiyaashree@users.noreply.github.com>
2024-01-21 13:29:01 +0530

    spacing aligment

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -6,31 +6,31 @@ export default function Activities()
                             <ul className="list-disc  tracking-wide">
                                 <div className="md:grid md:grid-cols-[5fr_1fr]">
                                     <li >
-                                    <span className="font-bold">Joint Secretary</span>
-                                        <p>Computer Science Engineering Coding Club - KEC</p>
+                                    <span className="font-bold py-1">Joint Secretary</span>
+                                        <p className="py-1">Computer Science Engineering Coding Club - KEC</p>
                                     </li>
                                     <div className="time_period">
-                                    <p className="flex items-center rounded-full bg-teal-300/70 px-4">2023-2024</p>
+                                    <p className="flex items-center rounded-full bg-teal-300/70 px-4 py-1">2023-2024</p>
                                     </div>
                                 </div>
                                 <br/>
                                 <div className="md:grid md:grid-cols-[5fr_1fr] ">
                                     <li >
-                                    <span className="font-bold">Class Representative</span>
-                                        <p >II CSE-B (3rd Semester) - KEC </p>
+                                    <span className="font-bold py-1">Class Representative</span>
+                                        <p className='py-1'>II CSE-B (3rd Semester) - KEC </p>
                                     </li>
                                     <div className="time_period">
-                                    <p className="flex items-center rounded-full bg-teal-300/70 px-4">2022-2022</p>
+                                    <p className="py-1 flex items-center rounded-full bg-teal-300/70 px-4">2022-2022</p>
                                     </div>
                                 </div>
                                 <br/>
                                 <div className="md:grid md:grid-cols-[5fr_1fr] ">
                                     <li> 
-                                    <span className="font-bold">Executive Member</span>
-                                    <p>Computer Science Engineering Coding Club - KEC</p>
+                                    <span className=" py-1 font-bold">Executive Member</span>
+                                    <p className="py-1">Computer Science Engineering Coding Club - KEC</p>
                                     </li>
                                     <div className="time_period">
-                                    <p className="flex items-center rounded-full bg-teal-300/70 px-4">2022-2023</p>
+                                    <p className="py-1 flex items-center rounded-full bg-teal-300/70 px-4">2022-2023</p>
                                     </div>
                                 </div>
 
